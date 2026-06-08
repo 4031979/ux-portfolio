2fee5de9b6150036c282f2de1d7e89943ce1aa87
davide.gomiero”git config --global user.email “gomiero.david@email.com <tua@email.com>
2026-06-08 11:23:26 +0200

    neeekkwfixes2

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import './HomePage.css';
 import solidformVideo from '../assets/solidform01.mp4';
 import telarycover from '../assets/cover-home.jpg';
-import nozomicover from '../assets/nozomi-thumb-home.png'; 
+// import nozomicover from '../assets/nozomi-thumb-home.png'; 
 import hanglagecover from '../assets/Exylus-targ-2.png'
 
 const HomePage = () => {
@@ -49,3 +49,26 @@ const ProjectCard = ({ title, category, link, image }) => (
 );
 
 export default HomePage;
+
+
+
+
+
+
+
+
+
+// import ProtectionUX from '../assets/Nozomi-UX-3.png'
+// import flowOnBoard from '../assets/flow-onBoard.png'
+// import DetailPage from '../assets/process-03.png'
+// import PlanConfig from '../assets/Plan-config.png'
+// import kiosk01 from '../assets/kiosk-01.png'
+// import kiosk02 from '../assets/kiosk-02.png'
+// import kiosk03 from '../assets/kiosk-03.png'
+// import target01 from '../assets/Target-01.png'
+// import target02 from '../assets/Target-02.png'
+// import nozomiSlide from '../assets/Slide-16_9-25-min.jpg';
+// import nozomiInstall1 from '../assets/install-01.png';
+// import nozomiInstall2 from '../assets/install-02.png';
+// import nozomiInstall4 from '../assets/install-04.png';
+// import nozomiInstallEnd from '../assets/install-end.png';
